ec2df47ceccfb307e74123bf4b1e3d39cbbe3c49
ahmed_khaled1 <ahmedkhaledmohamed9@gmail.com>
2021-12-11 09:14:30 +0200

    Creat a cache to store the parsed templates
    * CreateTemplateCache() parse all of our templates, including the layouts and store them in cache,
     which is actually a map that i can look up values in it by a string, and the string happens to be the name of the page(template) e.g. about.page.tmpl
     and once i look up them up, i get a pointer to that ready to use template, which exactly what i ask for to render pages on the site.
    * RenderTemplate() call the CreateTemplateCache()
    to access the cache
    pass the name of tmpl to the map to look up a parsed template, check if this tmpl is actually in the map
    aBuffer will store the executed template, because the template will be read from memory not from disk
    after executing the template pass the parsed file to the aBuffer as holder to WriteTo()  the web
    
    * To improve
    because every single time we visit a single page, will building  the template cache to render this individual page, we are actually parse all of our templates, that's because we are calling CreateTemplateCache() directly in the RenderTemplate()

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,18 +1,72 @@
 package render
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
+
+//var functions a map of functions that can be used in templates e.g. format a date
+// some time we will create our own functions and pass them to the template
+var functions = template.FuncMap{}
 
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
 
-	parsedTemplate, err := template.ParseFiles("../../templates/" + tmpl)
+	templateCache, err := CreateTemplateCache()
 	if err != nil {
-		fmt.Println("error parsing template: ", err)
-		return
+		log.Fatal(err)
 	}
-	parsedTemplate.Execute(w, nil)
+	theTemplate, ok := templateCache[tmpl]
+	if !ok {
+		log.Fatal(err)
+	}
 
+	aBuffer := new(bytes.Buffer)
+
+	_ = theTemplate.Execute(aBuffer, nil)
+
+	_, err = aBuffer.WriteTo(w)
+	if err != nil {
+		fmt.Println("Error writing to the browser", err)
+	}
+
+}
+
+// CreateTemplateCache return a map that has the parsed templates include the layouts
+func CreateTemplateCache() (map[string]*template.Template, error) {
+
+	cache := map[string]*template.Template{}
+
+	//filepath.Glob get the location of template pages.
+	pagesPath, err := filepath.Glob("../../templates/*.page.tmpl")
+	if err != nil {
+		return cache, err
+	}
+
+	// for loop extract the page name for the pages' path.
+	for _, page := range pagesPath {
+		pageName := filepath.Base(page)
+
+		templateSet, err := template.New(pageName).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return cache, err
+		}
+		// check template matches any layouts
+		matches, err := filepath.Glob("../../templates/*.layout.tmpl")
+		if err != nil {
+			return cache, err
+		}
+
+		if len(matches) > 0 {
+			templateSet, err = templateSet.ParseGlob("../../templates/*.layout.tmpl")
+			if err != nil {
+				return cache, err
+			}
+		}
+		cache[pageName] = templateSet
+	}
+	return cache, nil
 }
