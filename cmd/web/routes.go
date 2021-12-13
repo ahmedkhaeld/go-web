8115da8a80c6b5debb351029680348aa22eb979d
ahmed_khaled1 <ahmedkhaledmohamed9@gmail.com>
2021-12-13 04:39:19 +0200

    Use chi for routing instead pat

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,16 +3,24 @@ package main
 import (
 	"github.com/ahmedkhaeld/go-web/pkg/config"
 	"github.com/ahmedkhaeld/go-web/pkg/handlers"
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	//mux := pat.New()
+	//
+	//mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	//mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux := chi.NewRouter()
 
+	// Gracefully absorb panics and prints the stack trace
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 
 }
