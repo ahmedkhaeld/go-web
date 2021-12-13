d30a941083b4819dd095f15779eaff9774d34693
ahmed_khaled1 <ahmedkhaledmohamed9@gmail.com>
2021-12-13 07:28:39 +0200

    Sessions
    How to use Sessions
    Where  to use session? in main
    Where most likely to place session to get the required info? handlers
    put session management field in app wide access

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,15 +5,32 @@ import (
 	"github.com/ahmedkhaeld/go-web/pkg/config"
 	"github.com/ahmedkhaeld/go-web/pkg/handlers"
 	"github.com/ahmedkhaeld/go-web/pkg/render"
+	"github.com/alexedwards/scs/v2"
 	"log"
 	"net/http"
+	"time"
 )
 
 const portNumber = ":8080"
 
+// app entry point to access AppConfig
+var app config.AppConfig
+
+// declare the session var
+var session *scs.SessionManager
+
 func main() {
-	// app entry point to access AppConfig
-	var app config.AppConfig
+	// change this to true when in production environment
+	app.InProduction = false
+
+	// initialize the session
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.Secure = app.InProduction
+	session.Cookie.SameSite = http.SameSiteLaxMode
+
+	app.Session = session
 
 	templateCache, err := render.CreateTemplateCache()
 	if err != nil {
@@ -29,12 +46,8 @@ func main() {
 
 	render.NewTemplates(&app)
 
-	//http.HandleFunc("/", handlers.Repo.Home)
-	//http.HandleFunc("/about", handlers.Repo.About)
-
 	fmt.Println(fmt.Sprintf("Starting the application %v", portNumber))
 
-	//_ = http.ListenAndServe(portNumber, nil)
 	srv := &http.Server{
 		Addr:    portNumber,
 		Handler: routes(&app),
--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -5,14 +5,20 @@ import (
 	"net/http"
 )
 
+// NoSurf adds CSRF protection against all POST requests
 func NoSurf(next http.Handler) http.Handler {
 	csrfHandler := nosurf.New(next)
 
 	csrfHandler.SetBaseCookie(http.Cookie{
 		HttpOnly: true,
 		Path:     "/",
-		Secure:   false,
+		Secure:   app.InProduction,
 		SameSite: http.SameSiteLaxMode,
 	})
 	return csrfHandler
+}
+
+// SessionLoad loads and saves the session on every request for a middleware
+func SessionLoad(next http.Handler) http.Handler {
+	return session.LoadAndSave(next)
 }
